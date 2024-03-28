138aaae6d9409c6bbdfe076b0d705f5a6bb89789
brianlucasdev <brianlucasdias@gmail.com>
2024-03-28 17:04:09 -0300

    add divisivel

---
+++ divisivel.py
@@ -0,0 +1,6 @@
+num1  = float(input('digite o primeiro numero: '))
+num2  = float(input('digite o segundo numero: '))
+if num1 % num2 == 0:
+    print(num1,' é divisivel por ',num2)
+else:
+    print(num1,' não é divisivel por ',num2)
